ca6cf790b82bc4ba7d93e7fc9f289559a38c7aff
Aidan Sutherland <339501421@GoTVDSB.ca>
2024-03-21 14:22:57 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _12_Topic_6___Loops
+{
+    internal class Program
+    {
+        public static void prompter()
+        {
+            Console.WriteLine("1 - Prompter");
+        }
+
+        public static void percentPassing()
+        {
+            Console.WriteLine("2 - Percent Passing");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
